2fdba1e028be578ad2c73ec414c80cbff315c4f2
Simone_Berti <foto.berti55@gmail.com>
2022-03-17 09:18:29 +0100

    aggiunta di configurazioni

--- src/main/java/girolamo/linkdue/apirestboot/models/service/WebConfig.java
+++ src/main/java/girolamo/linkdue/apirestboot/models/service/WebConfig.java
@@ -11,6 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("POST", "PUT", "GET",  "DELETE", "OPTIONS")
+                .allowedHeaders("Content-Type", "Origin")
+                .exposedHeaders("X-Total-Count", "Location", "Access-Control-Allow-Origin")
+                .allowCredentials(false)
+                .maxAge(6000);
     }
 }
